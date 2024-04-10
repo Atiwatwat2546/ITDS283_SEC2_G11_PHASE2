f60757a64fa694a55fdd2b4f4ee07c0375e6b9e7
Atiwatwat2546 <atiwatwat2546@hmail.com>
2024-04-10 20:20:30 +0700

    First Commit

---
+++ lib/first_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'PW TO-DO APP',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: FirstPage(),
+  ));
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 49, 60, 128),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'PW',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'TO-DO',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'APP',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // เพิ่มโค้ดสำหรับการเข้าสู่ระบบที่นี่
+                },
+                child: Text('เข้าสู่ระบบ',
+                style: TextStyle(
+                      color: Color.fromARGB(255, 49, 60, 128),
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
