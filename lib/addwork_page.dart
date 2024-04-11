a28c4bc6fd61fd4cbf5e1a3eb1e3376875ff3290
Atiwatwat2546 <atiwatwat2546@gmail.com>
2024-04-12 00:08:57 +0700

    update

--- lib/addwork_page.dart
+++ lib/addwork_page.dart
@@ -129,6 +129,19 @@ class _TodoPageState extends State<TodoPage> {
                       ),
                     ),
                     SizedBox(height: 20),
+                    // TextButton(
+                    //   onPressed: () {
+                    //     showDatePicker(
+                    //       context: context,
+                    //       initialDate: DateTime.now(),
+                    //       firstDate: DateTime(1900),
+                    //       lastDate: DateTime.now(),
+                    //     ).then((pickedDate) {
+                          
+                    //     });
+                    //   },
+                    //   child: Text('Select Birthday'),
+                    // ),
                     Row(
                       children: [
                         Expanded(
