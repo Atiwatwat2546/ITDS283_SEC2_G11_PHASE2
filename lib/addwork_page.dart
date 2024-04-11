67b1145da7b66939c693cd5852c79eaf2abe64af
Atiwatwat2546 <atiwatwat2546@gmail.com>
2024-04-11 23:20:57 +0700

    creat addwork_page.dart
    update

---
+++ lib/addwork_page.dart
@@ -0,0 +1,322 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Todo Page',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: TodoPage(),
+  ));
+}
+
+class TodoPage extends StatefulWidget {
+  @override
+  _TodoPageState createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  final TextEditingController _activityController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _detailsController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Color.fromARGB(255, 49, 60, 128),
+        title: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'เพิ่มงาน',
+                  style: TextStyle(
+                    fontFamily: "Promt",
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 49, 60, 128),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 237, 237, 237),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(45),
+                  topRight: Radius.circular(45),
+                ),
+              ),
+            ),
+          ),
+          Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 49, 60, 128),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(45),
+                      topRight: Radius.circular(45),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 20,
+                right: 20,
+                top: 40,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0), // กำหนดระยะห่างด้านซ้ายของข้อความ
+                      child: Text(
+                        'ชื่อกิจกรรม',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Promt-SemiBold",
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    TextFormField(
+                      controller: _activityController,
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 15.0), // ปรับขนาดของช่องข้อความ
+                        hintText: 'ป้อนชื่อกิจกรรม',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 120, 154, 228),
+                        hintStyle: TextStyle(
+                          fontFamily: 'Promt-Thin',
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 206, 206, 206),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // โค้ดสำหรับเลือกวันที่
+                            },
+                            child: Text(
+                              'เลือกวันที่',
+                              style: TextStyle(
+                                fontFamily: "Promt-SemiBold",
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // โค้ดสำหรับเลือกเวลา
+                            },
+                            child: Text(
+                              'เลือกเวลา',
+                              style: TextStyle(
+                                fontFamily: "Promt-SemiBold",
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0), // กำหนดระยะห่างด้านซ้ายของข้อความ
+                      child: Text(
+                        'ที่อยู่',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Promt-SemiBold",
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    TextFormField(
+                      controller: _addressController,
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 15.0), // ปรับขนาดของช่องข้อความ
+                        hintText: 'ป้อนทีอยู่',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 120, 154, 228),
+                        hintStyle: TextStyle(
+                          fontFamily: 'Promt-Thin',
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 206, 206, 206),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0), // กำหนดระยะห่างด้านซ้ายของข้อความ
+                      child: Text(
+                        'รายละเอียด',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Promt-SemiBold",
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    TextFormField(
+                      controller: _detailsController,
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 15.0), // ปรับขนาดของช่องข้อความ
+                        hintText: 'ป้อนรายละเอียด',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 120, 154, 228),
+                        hintStyle: TextStyle(
+                          fontFamily: 'Promt-Thin',
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 206, 206, 206),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // โค้ดสำหรับบันทึกข้อมูล
+                              String activity = _activityController.text;
+                              String address = _addressController.text;
+                              String details = _detailsController.text;
+                              // นำข้อมูลไปที่ Firebase หรือทำอย่างอื่นตามต้องการ
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromARGB(255, 131, 193, 81)),
+                            child: Text(
+                              'บันทึก',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // โค้ดสำหรับยกเลิก
+                              Navigator.pop(context);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 177, 67, 67),
+                            ),
+                            child: Text(
+                              'ยกเลิก',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 160,
+                bottom: 35,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Color.fromARGB(255, 154, 120, 255),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.person,
+                          size: 55,
+                        ),
+                        onPressed: () {
+                          // โค้ดสำหรับการเปลี่ยนหน้า
+                          Navigator.pushNamed(context, '/todo');
+                        },
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
